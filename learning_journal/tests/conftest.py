691d350b183a9f2438d5c8be9221164db8653c53
Kyle R <KyleRichVA@gmail.com>
2016-03-15 17:47:19 -0700

    Added Entry Model and tested it's creation into a postsql server

---
+++ learning_journal/tests/conftest.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import pytest
+from learning_journal.models import DBSession, Base
+from sqlalchemy import create_engine
+
+
+TEST_DATABASE_URL = 'postgres://macuser:@localhost:5432/test_journal'
+
+
+@pytest.fixture(scope='session')
+def sqlengine(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        Base.metadata.drop_all(engine)
+    request.addfinalizer(teardown)
+    return engine
+
+
+@pytest.fixture()
+def dbtransaction(request, sqlengine):
+    connection = sqlengine.connect()
+    transaction = connection.begin()
+    DBSession.configure(bind=connection)
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        DBSession.remove()
+
+    request.addfinalizer(teardown)
+
+    return connection
+
+@pytest.fixture()
+def session(dbtransaction):
+    from learning_journal.models import DBSession
+    return DBSession
