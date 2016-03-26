6815af5870176df3cadae094e9538fca8be71c5d
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-26 15:26:29 -0700

    created files for deployment

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -1,24 +1,24 @@
+"""Initialize DB."""
 import os
 import sys
-import transaction
 
 from sqlalchemy import engine_from_config
 
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    Entry,
     Base,
-    )
+)
 
 
 def usage(argv):
+    """Print usage to command line."""
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd))
@@ -26,17 +26,19 @@ def usage(argv):
 
 
 def main(argv=sys.argv):
+    """Set up engine and session."""
     if len(argv) < 2:
         usage(argv)
+
     config_uri = argv[1]
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
+
+    database_url = os.environ.get('DATABASE_URL', None)
+    if database_url is not None:
+        settings['sqlalchemy.url'] = database_url
+
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        entry1 = Entry(title=u"Here is a test entry.", text=u"I am a great test.")
-        DBSession.add(entry1)
-        entry2 = Entry(title=u"Oh hey here is another entry.", text=u'This is a better test.')
-        DBSession.add(entry2)
